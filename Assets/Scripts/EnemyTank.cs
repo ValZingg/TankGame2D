8a9e63f0cdd3076653bf9838186e618a4e7d61c7
ValZ <48352407+ValZingg@users.noreply.github.com>
2022-05-13 08:21:45 +0200

    Premier push du système d'IA

--- Assets/Scripts/EnemyTank.cs
+++ Assets/Scripts/EnemyTank.cs
@@ -25,6 +25,11 @@ public class EnemyTank : MonoBehaviour
     public float ShootCoolDown = 1.0f; //Temps de recharge entre chaque tir
     public bool IsLoaded = true; //Est-ce qu'un obus est chargé ?
 
+    [Header("Intelligence Artificielle")]
+    public string CurrentMode = "Patrol"; //Comportement actual, ex : "Patrouille, poursuite"
+    public GameObject CurrentWayPoint; //Point dans laquelle le tank est en train de se diriger
+    public CircleCollider2D SightRange; //Distance où le tank peut voir 
+
 
     [Header("GameObjects")]
     public GameObject CenterPoint;
@@ -72,7 +77,35 @@ public class EnemyTank : MonoBehaviour
 
     void Update()
     {
-        //TODO : Intelligence artificielle
+        if(CurrentWayPoint != null)
+        {
+            Vector3 dirFromAtoB = (CurrentWayPoint.transform.position - TankBody.transform.TransformDirection(TankBody.transform.position)).normalized;
+            float dotProd = Vector3.Dot(dirFromAtoB, TankBody.transform.TransformDirection(TankBody.transform.forward));
+
+            if (dotProd > 0.9)
+            {
+                ControlTank("accelerate");
+            }
+            else ControlTank("turnright");
+
+            Debug.Log(dotProd);
+        }
+
+        //MOUVEMENT DU TANK
+        //Bouge le tank
+        if (currentspeed != 0) transform.position += TankBody.transform.right * Time.deltaTime * currentspeed;
+
+        //Corrige les erreurs de vitesse
+        if (currentspeed > tankspeed) currentspeed = tankspeed;
+        if (currentspeed < -tankspeed) currentspeed = -tankspeed;
+    }
+
+    public void ControlTank(string order) //Contrôle le tank suivant l'ordre (ex : accélérer, décélerer, tourner...)
+    {
+        if(order == "accelerate") currentspeed += acceleration * Time.deltaTime;
+        if(order == "slowdown") currentspeed -= acceleration * Time.deltaTime;
+        if(order == "turnright") TankBody.transform.Rotate(Vector3.back * tankrotatespeed * Time.deltaTime);
+        if(order == "turnleft") TankBody.transform.Rotate(Vector3.forward * tankrotatespeed * Time.deltaTime);
     }
 
     public void Explode() //Explosion du tank lorsqu'il est détruit
